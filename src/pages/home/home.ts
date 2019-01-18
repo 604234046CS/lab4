44a89918ae89b382459353d1895124a5949f36de
604234046CS <604234046cs@parichat.skru.ac.th>
2019-01-18 18:56:20 +0700

    lab4

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
+import { BookListPage } from './../book-list/book-list';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -8,6 +10,15 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+
+  }
+
+  showBookList(categoryName:string){
+    console.log(categoryName);
+    this.navCtrl.push(BookListPage, 
+      {category:categoryName}
+      );
+
 
   }
 
